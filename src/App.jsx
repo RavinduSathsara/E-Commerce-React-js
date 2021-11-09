1e37035fdfc5a2259d25332df2d129563406e4b6
Ravindu_Sathsara <rsathsara330@gmail.com>
2021-11-09 23:53:46 +0530

    Add Footer for Appication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import AppNavBar from "./components/AppNavBar";
+import Footer from "./components/Footer";
 import Home from "./screens/Home";
 
 const App = () => {
   return (
     <div>
       <AppNavBar />
-
       <div className="container bg-light">
         <Home />
       </div>
+      {/* footer */}
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      {/* Footer */}
+      <footer class="py-5 bg-dark">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; E-Commerce 2021
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
