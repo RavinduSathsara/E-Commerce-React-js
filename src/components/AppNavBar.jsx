183e5a03ac2025450688e1bb09e5ad97fd9aa85e
Ravindu_Sathsara <rsathsara330@gmail.com>
2021-11-09 23:23:19 +0530

    Create Home Page

--- src/components/AppNavBar.jsx
+++ src/components/AppNavBar.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const AppNavBar = () => {
   return (
     <div className="font-weight-bold">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 fixed-top ">
-        <div class="container">
-          <a class="navbar-brand " href="#">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 fixed-top ">
+        <div className="container">
+          <a className="navbar-brand " href="#">
             E - Commerce
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
@@ -17,31 +17,31 @@ const AppNavBar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
 
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
                   Features
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
                   Pricing
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">
+              <li className="nav-item">
+                <a className="nav-link disabled">
                   <a href="">
-                    <i class="bi bi-cart  mx-2">
-                      <span class="badge bg-dark mx-1 ">2</span>
+                    <i className="bi bi-cart  mx-2">
+                      <span className="badge bg-dark mx-1 ">3</span>
                     </i>
                   </a>
                 </a>
